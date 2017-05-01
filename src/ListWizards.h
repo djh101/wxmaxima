65eaa9584f8254854f1135f54b7570394ded78e2
Gunter Königsmann <gunter@peterpall.de>
2017-05-01 17:27:20 +0200

    Do we need more wizards?

---
+++ src/ListWizards.h
@@ -0,0 +1,32 @@
+﻿// -*- mode: c++; c-file-style: "linux"; c-basic-offset: 2; indent-tabs-mode: nil -*-
+//
+//  Copyright (C) 2004-2015 Andrej Vodopivec <andrej.vodopivec@gmail.com>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#ifndef __LISTWIZARDS_H__
+#define __LISTWIZARDS_H__
+
+#include <wx/dialog.h>
+
+class CreateList : public wxDialog
+{
+public:
+  CreateList(wxWindow *parent, int id);
+};
+
+#endif
